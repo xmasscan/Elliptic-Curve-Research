e0f1af0b730b82933a3c67e49fb660829b91bf6d
xmasscan <mkhan398@uic.edu>
2025-03-17 11:45:22 -0500

    Determinant calculation in addition, fancy pritning in main :)

---
+++ addition.py
@@ -0,0 +1,77 @@
+#
+#   Finding Rational Points on Elliptic Curves
+#   MATH 496 : Independent Study @ UIC
+#   Author: Michael Hanif Khan 
+#   
+#   This program takes coefficients a1 -> a6 and two points.
+#   Based on these inputs, whether or not the curve is ellpitic is checked and addition is performed.
+
+import fractions as frac
+
+########################
+#
+#   calculate_determinant(a1,a2,a3,a4,a5,a6)
+#   a1 ... a6 : Fraction Objects
+#   
+#   calculates weights b2,b4,b6, and b8.
+#   uses weights to calculate the determinant
+#
+########################
+
+# Weierstrass Equations for algerbraically describing Elliptic Curve Addition
+# See Silverman's "The Arithmetic of Elliptic Curves" Page 42
+
+# a is an array of the coefficients which are labeled as "a_i" where i is the index starting from 1.
+# for clarity, indicies will be written as their 1-indexed index minus one. :)
+# every element in a is a fraction.
+
+# a = List of Weierstrass Weights input in the order:
+# a_1, a_3, a_2, a_4, a_6
+# Therefore:
+# a_1 = a[0]
+# a_3 = a[1]
+# a_2 = a[2]
+# a_4 = a[3]
+# a_6 = a[4]
+
+def calculate_determinant(a):
+    # assuming a is an array of integers where as are passsed in order specified here:
+    # a_1, a_3, a_2, a_4, a_6
+
+    b2 = pow(a[0],2) + 4 * a[2]
+    b4 = 2 * a[3] + a[0] * a[1]
+    b6 = pow(a[1],2) + 4 * a[4]
+    b8 = pow(a[0],2) * a[4] + 4 * a[2] * a[4] - a[0] * a[1] * a[3] + a[2] * pow(a[1],2)
+
+    return -pow(b2,2) * b8 - 8 * pow(b4,3) - 27 * pow(b6,2) + 9 * b2 * b4 * b6
+
+# See Silverman's "The Arithmetic of Elliptic Curves" Page 54
+# lambda is a Python built in, so we will call it "l".
+
+# a_1 = a[0]
+# a_3 = a[1]
+# a_2 = a[2]
+# a_4 = a[3]
+# a_6 = a[4]
+def add_points(a, x1: frac.Fraction, y1: frac.Fraction, x2: frac.Fraction, y2: frac.Fraction):
+    if x1 == x2:
+        l_num = 3 * pow(x1,2) + 2 * a[2] * x1 + a[3] - a[0] * y1
+        l_denom = 2 * y1 + a[0] * x1 + a[1]
+        l = frac.Fraction(l_num,l_denom)
+
+        v_num = -1 * pow(x1,3) + a[3] * x1 + 2 * a[4] - a[1] * y1
+        v_denom = 2 * y1 + a[0] * x1 + a[1]
+        v = frac.Fraction(v_num, v_denom)
+    else:
+        l_num = 3 * pow(x1,2) + 2 * a[2] * x1 + a[3] - a[0] * y1
+        l_denom = 2 * y1 + a[0] * x1 + a[1]
+        l = frac.Fraction(l_num,l_denom)
+
+        v_num = -1 * pow(x1,3) + a[3] * x1 + 2 * a[4] - a[1] * y1
+        v_denom = 2 * y1 + a[0] * x1 + a[1]
+        v = frac.Fraction(v_num, v_denom)
+
+    x3 = pow(l,2) + a[0] * l - a[2] - x1 - x2
+    y3 = -(l + a[0]) * x3 - v - a[1]
+    point = (x3,y3)
+    return point
---
+++ main.py
@@ -0,0 +1,73 @@
+import addition as add
+
+
+# Pretty Weierstrass Equation printing
+def print_weierstrass(a):
+    # All terms zeroable by respective weights
+    terms = ["xy","y","x^2","x"]
+    # Begin building equation
+    weierstrass = "y^2" 
+    # if a_1 != 0
+    if a[0] != 0:
+        # just print the term if a_1 = 1
+        if a[0] == 1:
+            weierstrass += " + xy"
+        else:
+            weierstrass += f" + {a[0]}xy"
+    # a_3 handling
+    if a[1] != 0:
+        # just print the term if a_1 = 1
+        if a[1] == 1:
+            weierstrass += " + y"
+        else:
+            weierstrass += f" + {a[1]}y"
+
+    # left side complete, begin right side
+    weierstrass += " = x^3"
+
+    if a[2] != 0:
+        if a[2] == 1:
+            weierstrass += " + x^2"
+        else:
+            weierstrass += f" + {a[2]}x^2"
+    if a[3] != 0:
+        if a[3] == 1:
+            weierstrass += " + x"
+        else:
+            weierstrass += f" + {a[3]}x"
+    if a[4] != 0:
+        weierstrass += f" + {a[4]}"
+
+    print(weierstrass)
+
+# a_1, a_3, a_2, a_4, a_6 in that order :)
+a = ["a_1","a_3","a_2","a_4","a_6"]
+weights = []
+user_input_flag = False
+
+while not user_input_flag:
+    print("Input the weights for a given Weierstrass Equation of the form:")
+    print("y^2 + a_1 xy + a_3y = x^3 + a_2x^2 + a_4x + a_6")
+    for a_i in a:
+        weights.append(int(input(f"{a_i}: ")))
+    print()
+    print("This will produce the equation:")
+    print_weierstrass(weights) 
+    print()
+    response = input("Is this correct? (y/n): ")
+    if response.upper() == "Y":
+        user_input_flag = True
+    else:
+        print("Reprompting...")
+        print()
+        weights = []
+
+print()
+
+determinant = add.calculate_determinant(weights)
+if determinant == 0:
+    print("Determinant of specified function is zero.")
+    print("Aborting...")
+else:
+    print(f"Determinant: {determinant}")
+    print("Determinant non-zero, must be legal Elliptic Curve")
